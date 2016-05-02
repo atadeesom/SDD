ed99ab3345477b80703813046448caca908ac44e
Alexander Magmai <chanaphan.brillo@gmail.com>
2016-05-02 18:08:48 +0700

    remove to function report/display_section_report

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -28,23 +28,7 @@ class Report extends CI_Controller{
 	public function display_class_report(){
 		//TODO: implement function and return data to view.
 	}
-	
-	/**
-	 * Function display_section_report uses to gather data from db then
-	 * manipulate data to view.
-	 * This function is for teacher user's role.
-	 */
-	public function display_section_report(){
-		//TODO: implement function and return data to view.
-		$page_element['page_title'] = "Report";
-		$page_element['method_name'] = "Index";
-		$data['title'] = 'Report';
-		
-		// return data to view
-		$this->load->view('template/report_header',$page_element);
-		$this->load->view('report/teacher_report_section',$data);
-		$this->load->view('template/report_footer',$data);
-	}
+
 	
 	/**
 	 * Function display_student_report uses to gather data from db then
